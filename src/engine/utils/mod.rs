5ca0409928ef57fd09e8d00ffb7608e908896694
Ian Pratt <ianjdpratt@gmail.com>
2023-01-21 21:18:34 -0600

    smol changes

--- src/engine/utils/mod.rs
+++ src/engine/utils/mod.rs
@@ -18,12 +18,25 @@ impl Engine {
     /// Gets the cost of moving to a certain point, assuming that the point you are moving
     /// from is a neighbor of the point you are moving to.
     pub fn cost(&self, point: &Point) -> i32 {
+        let mut cost = 1;
+
         if self.board.food.contains(point) {
-            // If the point has food, it is a valid move, but with a cost of -1.
-            -5
-        } else {
-            // If the point is empty, it is a valid move, but with a cost of 0.
-            1
+            // If the point is a food, then it is a good move, so it is rewarded accordingly.
+            cost -= 5;
         }
+
+        if self
+            .board
+            .snakes
+            .iter()
+            .flat_map(|s| s.head.neighbors())
+            .any(|p| &p == point)
+        {
+            // If the point crosses right in front of a snake's head, it is a valid, but risky, move, so it
+            // is punished accordingly.
+            cost += 10;
+        }
+
+        cost
     }
 }
